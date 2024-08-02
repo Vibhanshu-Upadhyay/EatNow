c58e8ab986d2548aec759b44d1ee67069b7806ef
Vibhanshu-Upadhyay <vupadhyay2003@gmail.com>
2024-08-02 22:32:47 +0530

    mongo uri to .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://vupadhyay:gXG07v4yeNsxBqSF@cluster0.fqltn6v.mongodb.net/eatNow",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("My DB is now connected");
   } catch (err) {
     console.error("DB connection error:", err);
