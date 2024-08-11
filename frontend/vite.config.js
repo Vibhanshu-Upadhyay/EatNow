9db4785e57df042ac61f63a497528889bbeead3d
Vibhanshu-Upadhyay <vupadhyay2003@gmail.com>
2024-08-12 00:46:16 +0530

    rebuild the frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Ensure assets are correctly located in production
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
-    }
-  }
-})
+      "/api": "http://localhost:3000", // Only used during development
+    },
+  },
+});
