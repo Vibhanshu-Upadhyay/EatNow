85162ac8a10fd572dea3fda6368a1ebf19b0c41d
Vibhanshu-Upadhyay <vupadhyay2003@gmail.com>
2024-08-11 18:16:38 +0530

    cloudinary implemented

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,21 +1,36 @@
-import express from 'express'
-import { addFood, listFood, removeFood } from '../controllers/foodController.js'
-import multer from 'multer'
+import express from "express";
+import {
+  addFood,
+  listFood,
+  removeFood,
+} from "../controllers/foodController.js";
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+
+// Configure Cloudinary with your credentials
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // replace with your Cloudinary cloud name
+  api_key: process.env.CLOUDINARY_API_KEY, // replace with your Cloudinary API key
+  api_secret: process.env.CLOUDINARY_API_SECRET, // replace with your Cloudinary API secret
+});
+
+// Create a Cloudinary storage engine
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "food_images", // replace with the folder name where you want to store images
+    format: async (req, file) => "png", // supports promises as well
+    public_id: (req, file) => `${Date.now()}-${file.originalname}`, // optional, otherwise Cloudinary will generate an ID
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const foodRouter = express.Router();
 
-// Image Storage Engine
-
-const storage = multer.diskStorage({
-    destination:"uploads",
-    filename: (req,file,cb)=>{
-        return cb(null,`${Date.now()}${file.originalname}`)
-    }
-})
-
-const upload = multer({storage:storage})
-foodRouter.post('/add',upload.single('image'),addFood)
-foodRouter.get('/list',listFood)
-foodRouter.post('/remove', removeFood)
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
+foodRouter.post("/remove", removeFood);
 
 export default foodRouter;
